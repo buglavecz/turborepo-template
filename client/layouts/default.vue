41637aab5ca79a735f046603a03ae2d0f54404b7
Peter Buglavecz <buggio.dev@gmail.com>
2025-07-12 23:53:22 +0200

    feat: try 1

---
+++ client/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <CustomLayout>
+    <nuxt />
+  </CustomLayout>
+</template>
+
+<script>
+import CustomLayout from '../../frontend/nuxt3/layouts/default.vue'
+
+export default {
+  components: {
+   // CustomLayout
+  }
+}
+</script>
---
+++ frontend/nuxt3/layouts/default.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+</script>
+
+<template>
+<div>
+  <slot />
+</div>
+</template>
+
+<style scoped>
+
+</style>
