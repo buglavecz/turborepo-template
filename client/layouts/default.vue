95dd5fddd974b097b702a3662330282a2c2e9888
Peter Buglavecz <buggio.dev@gmail.com>
2025-07-17 22:58:00 +0200

    dev

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -4,6 +4,12 @@
   </default-layout>
 </template>
 
-<script setup>
+<script lang="ts">
 import DefaultLayout from 'n3-layouts/default.vue'
+import { defineComponent } from 'vue';
+export default defineComponent({
+  components: {
+    DefaultLayout
+  }
+})
 </script>
